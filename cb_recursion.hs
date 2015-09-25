69491928fa9278d1e83cbcf850b1eff7cffe6f88
unaveed <unaveed@gmail.com>
2015-09-24 22:36:49 -0600

    Implemented addDigits from LeetCode

--- cb_recursion.hs
+++ cb_recursion.hs
@@ -1,3 +1,5 @@
+main = print (addDigits 451)
+
 -- factorial
 fac n | n <= 1 = 1 | otherwise = n * fac(n - 1)
 
@@ -12,3 +14,6 @@ bunnyEars2 i =
 -- count7
 count7helper x | x == 7 = 1 | otherwise = 0
 count7  n | n < 7 = 0 | otherwise  = count7helper(n `mod` 10) + count7(quot n 10)
+
+-- add digits
+addDigits n = if n < 10 then n else (n `mod` 10) + addDigits(n `div` 10)
